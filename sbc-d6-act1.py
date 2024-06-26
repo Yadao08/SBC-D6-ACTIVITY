d127f3641edad79041f8374f37ffd93ad4cd81c8
Yadao08 <90585871+Yadao08@users.noreply.github.com>
2024-06-26 09:10:07 +0800

    Update sbc-d6-act1.py

--- sbc-d6-act1.py
+++ sbc-d6-act1.py
@@ -1,13 +1,13 @@
-user_input = input("Enter your word: ").strip().title()
 p = []
+user_input = input("Enter the word: ")
+palindrome = "".join(reversed(user_input.replace(" ","").upper()))
 
-# Reverse
-palindrome = ""
-for w in reversed(user_input):
-    palindrome += w
-    p.append(w)  # Append each character to the list P
-if user_input == palindrome:
-    print("Palindrome")
-else:
-    print("Not a palindrome")
-print("Reversed word:", ''.join(p))
+rep = user_input.replace(" ","").upper()
+p.append(rep)
+
+for i in p:
+    if i == palindrome:
+       print( user_input, "Is a Palindrome" )
+    else:
+       print( user_input,"Is not a palindrome",)
+print("Reversed word:", '',palindrome)
